6447560e0524701d1e02a468b18bd2181de3a463
Xie Yuheng <xyheme@gmail.com>
2023-03-16 13:34:40 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { host: '0.0.0.0' },
   test: { environment: 'jsdom' },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          postmark: ['@xieyuheng/postmark'],
-          highlight: ['highlight.js'],
-          vue: ['vue'],
-          'sanitize-html': ['sanitize-html'],
-        },
-      },
-    },
-  },
-
-  plugins: [vue(), pluginRewriteAll()],
+  build: { sourcemap: true },
+  plugins: [vue(), splitVendorChunkPlugin(), pluginRewriteAll()],
 })
