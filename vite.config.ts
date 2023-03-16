a10683c00f3c6166636e10f2344d4acfc616007a
Xie Yuheng <xyheme@gmail.com>
2023-03-16 13:03:10 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { splitVendorChunkPlugin, defineConfig } from 'vite'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
@@ -9,7 +9,7 @@ export default defineConfig({
   build: { sourcemap: true },
   plugins: [
     vue(),
-
+    splitVendorChunkPlugin(),
     // Vite 2 plugin that fix dev server not rewriting the path includes a dot.
     // - repo: https://github.com/ivesia/vite-plugin-rewrite-all
     // - issues:
