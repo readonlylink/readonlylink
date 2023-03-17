33f75b3a95ef68f242c969931dfeb17696b8eb61
Xie Yuheng <xyheme@gmail.com>
2023-03-17 17:19:32 +0800

    up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-git pull && pnpm i && pnpm run build && sudo rm -rf /websites/readonlylink && sudo cp -r dist /websites/readonlylink
+git pull && npm i && npm run build && sudo rm -rf /websites/readonlylink && sudo cp -r dist /websites/readonlylink
