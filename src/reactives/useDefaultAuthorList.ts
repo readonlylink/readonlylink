214aa16895b575d95b66cd44082b9ddcc486b8b3
Xie Yuheng <xyheme@gmail.com>
2023-03-19 21:14:27 +0800

    up

--- src/reactives/useDefaultAuthorList.ts
+++ src/reactives/useDefaultAuthorList.ts
@@ -2,5 +2,6 @@ export function useDefaultAuthorList(): Array<string> {
   return [
     'https://readonly.link/contents/author.json',
     'https://inner.xieyuheng.com/author.json',
+    'https://shadowrz.github.io/author.json',
   ]
 }
