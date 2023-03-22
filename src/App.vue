d993115bc966faf55ff551174cf2a67cbbe22a1d
Xie Yuheng <xyheme@gmail.com>
2023-03-22 20:29:35 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import { Head } from '@vueuse/head'
+import { useGlobalTheme } from './reactives/useGlobalTheme'
+
+const theme = useGlobalTheme()
+</script>
+
 <template>
+  <Head>
+    <meta name="theme-color" :content="theme.color" />
+  </Head>
+
   <RouterView />
 </template>
