3a8e89ed6795c4390365376b03374dc7cbeafe3e
Xie Yuheng <xyheme@gmail.com>
2023-03-22 20:32:20 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 import { Head } from '@vueuse/head'
-import { useGlobalTheme } from './reactives/useGlobalTheme'
-
-const theme = useGlobalTheme()
 </script>
 
 <template>
   <Head>
-    <meta name="theme-color" :content="theme.color" />
+    <meta name="theme-color" content="white" />
   </Head>
 
   <RouterView />
