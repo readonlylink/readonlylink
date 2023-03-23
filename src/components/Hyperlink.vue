c78d4235ac2f672f4a0f6e02c112f0f60ccf9684
Xie Yuheng <xyheme@gmail.com>
2023-03-24 03:05:54 +0800

    up

--- src/components/Hyperlink.vue
+++ src/components/Hyperlink.vue
@@ -1,18 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// We use our custom <Hyperlink> instead of <RouterLink>,
+// so that we can add some behaviors to every links.
+
+const props = defineProps<{
+  href: string
+}>()
+</script>
+
+<template>
+  <RouterLink v-bind="props" :to="props.href"><slot /></RouterLink>
+</template>
 
 <!--
-
-     We use our custom <Hyperlink> instead of <RouterLink>,
-     so that we can add some behaviors to every links.
 
      For example, we can not use <RouterLink> in headlessui:
      - https://github.com/tailwindlabs/headlessui/issues/143
 
 -->
-
-<template>
-  <RouterLink v-bind="$attrs" :to="$attrs.href"><slot /></RouterLink>
-</template>
 
 <!--
      <template>
