d7b2baae804d22a1e0602a28a64bc25e78a9d851
Xie Yuheng <xyheme@gmail.com>
2023-03-28 05:59:40 +0800

    remove v-tooltip

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { createHead } from '@vueuse/head'
-import FloatingVue from 'floating-vue'
-import 'floating-vue/dist/style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import * as Directives from './directives'
@@ -12,7 +10,6 @@ const app = createApp(App)
 
 app.use(router)
 app.use(createHead())
-app.use(FloatingVue)
 
 app.directive('focus', Directives.focus)
 app.directive('blur', Directives.blur)
