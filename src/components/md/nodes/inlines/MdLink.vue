9663e2d0d22aac0e7ea7467c358a5a2c035efdd1
Xie Yuheng <xyheme@gmail.com>
2023-04-14 22:17:00 +0800

    up

--- src/components/md/nodes/inlines/MdLink.vue
+++ src/components/md/nodes/inlines/MdLink.vue
@@ -15,7 +15,7 @@ defineProps<{
 <template>
   <a
     v-if="isExternalLink(node.href)"
-    class="break-words text-sky-600 underline decoration-sky-600 visited:text-purple-600 visited:decoration-purple-600"
+    class="break-words text-sky-700 underline decoration-sky-700 visited:text-purple-700 visited:decoration-purple-700"
     :href="node.href"
     :title="node.title"
     target="_blank"
@@ -32,7 +32,7 @@ defineProps<{
 
   <Hyperlink
     v-else
-    class="break-words text-sky-600 underline decoration-sky-600 visited:text-purple-600 visited:decoration-purple-600"
+    class="break-words text-sky-700 underline decoration-sky-700 visited:text-purple-700 visited:decoration-purple-700"
     :href="node.href"
     :title="node.title"
   >
