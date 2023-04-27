7f1cec68aca8963c4c4a51b64c493b3cf87b6cc3
Xie Yuheng <xyheme@gmail.com>
2023-04-27 14:13:48 +0800

    up

--- src/shims/vue.d.ts
+++ src/shims/vue.d.ts
@@ -1,5 +1,5 @@
-declare module '*.vue' {
-  import type { Component } from 'vue'
-  const component: Component
-  export default component
-}
+// declare module '*.vue' {
+//   import type { Component } from 'vue'
+//   const component: Component
+//   export default component
+// }
