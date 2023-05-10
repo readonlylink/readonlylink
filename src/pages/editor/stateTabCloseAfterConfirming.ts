2f3dcaff48edd9e84726b16fe129e0fa4a16180b
Xie Yuheng <xyheme@gmail.com>
2023-05-10 23:17:11 +0800

    up

--- src/pages/editor/stateTabCloseAfterConfirming.ts
+++ src/pages/editor/stateTabCloseAfterConfirming.ts
@@ -10,8 +10,8 @@ export async function stateTabCloseAfterConfirming(
   const lang = useGlobalLang()
 
   const message = lang.isZh()
-    ? '确认要关闭这个标签吗？\n有未保存的修改哦！\n${tab.file.name}'
-    : 'Are you sure to close this tab?\nThere are unsaved changes!\n${tab.file.name}'
+    ? `确认要关闭这个标签吗？\n有未保存的修改哦！\n${tab.file.name}`
+    : `Are you sure to close this tab?\nThere are unsaved changes!\n${tab.file.name}`
 
   if (window.confirm(message)) {
     await stateTabClose(state, tab)
