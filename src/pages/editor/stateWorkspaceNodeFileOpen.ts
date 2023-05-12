e9b582f32d56c2fad16b6f14a1d3d35d54780880
Xie Yuheng <xyheme@gmail.com>
2023-05-13 00:00:58 +0800

    up

--- src/pages/editor/stateWorkspaceNodeFileOpen.ts
+++ src/pages/editor/stateWorkspaceNodeFileOpen.ts
@@ -7,7 +7,16 @@ export async function stateWorkspaceNodeFileOpen(
   node: WorkspaceNodeFile,
 ): Promise<void> {
   const tab = await tabFromFileHandle(node.handle)
-  node.handle
-  state.tabs.push(tab)
-  state.currentTab = tab
+  tab.relativePath = node.relativePath
+
+  const foundTab = state.tabs.find(
+    (exitingTab) => exitingTab.relativePath === tab.relativePath,
+  )
+
+  if (foundTab) {
+    state.currentTab = foundTab
+  } else {
+    state.tabs.push(tab)
+    state.currentTab = tab
+  }
 }
