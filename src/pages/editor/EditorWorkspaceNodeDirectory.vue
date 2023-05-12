ca5b4b2de94034ed8067e7699879edc55d82d66b
Xie Yuheng <xyheme@gmail.com>
2023-05-13 00:29:43 +0800

    [editor] `EditorWorkspaceNodeDirectory` show open state

--- src/pages/editor/EditorWorkspaceNodeDirectory.vue
+++ src/pages/editor/EditorWorkspaceNodeDirectory.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { FolderIcon } from '@heroicons/vue/24/outline'
+import { FolderIcon, FolderOpenIcon } from '@heroicons/vue/24/outline'
 import EditorWorkspaceNode from './EditorWorkspaceNode.vue'
 import { State } from './State'
 import { Workspace } from './Workspace'
@@ -19,12 +19,14 @@ defineProps<{
       class="flex w-full shrink-0 items-center space-x-1 overflow-x-auto whitespace-pre px-3 hover:bg-stone-200"
       @click="stateWorkspaceNodeDirectoryOpen(state, node)"
     >
-      <FolderIcon class="h-4 w-4 shrink-0" />
+      <FolderOpenIcon v-if="node.isOpen" class="h-4 w-4 shrink-0" />
+      <FolderIcon v-else class="h-4 w-4 shrink-0" />
       <span>{{ node.handle.name }}<span class="font-bold">/</span></span>
     </button>
 
     <div v-if="node.children.length > 0" class="flex h-full flex-col pl-3">
       <EditorWorkspaceNode
+        class="border-l border-black"
         v-for="(child, index) of node.children"
         :key="index"
         :state="state"
