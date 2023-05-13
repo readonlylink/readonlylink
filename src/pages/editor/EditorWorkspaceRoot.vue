a1fee3b16dcaab2db285618a7ec79b66832e21a1
Xie Yuheng <xyheme@gmail.com>
2023-05-13 23:44:59 +0800

    up

--- src/pages/editor/EditorWorkspaceRoot.vue
+++ src/pages/editor/EditorWorkspaceRoot.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useGlobalLang } from '../../components/lang/useGlobalLang'
 import { State } from './State'
 import { Workspace } from './Workspace'
 
@@ -6,10 +7,13 @@ defineProps<{
   state: State
   workspace: Workspace
 }>()
+
+const lang = useGlobalLang()
 </script>
 
 <template>
-  <div class="px-3 py-1 font-bold">
-    {{ workspace.root.handle.name }}
+  <div class="flex justify-between space-x-2 py-1 pl-3 pr-1">
+    <div class="font-bold">{{ workspace.root.handle.name }}</div>
+    <div class="flex space-x-1"></div>
   </div>
 </template>
