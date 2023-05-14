00b49c150a12f85bfb8564b977105bb1f4708f20
Xie Yuheng <xyheme@gmail.com>
2023-05-14 19:05:58 +0800

    [editor] extract `EditorWelcomeStart`

--- src/pages/editor/EditorWelcome.vue
+++ src/pages/editor/EditorWelcome.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
-import {
-  DocumentPlusIcon,
-  DocumentTextIcon,
-  FolderIcon,
-} from '@heroicons/vue/24/outline'
 import Lang from '../../components/lang/Lang.vue'
+import EditorWelcomeStart from './EditorWelcomeStart.vue'
 import { State } from './State'
-import { stateDirectoryOpen } from './stateDirectoryOpen'
-import { stateFileCreate } from './stateFileCreate'
-import { stateFileOpen, stateOpenFileHandle } from './stateFileOpen'
+import { stateOpenFileHandle } from './stateFileOpen'
 
 defineProps<{ state: State }>()
 </script>
@@ -31,47 +25,7 @@ defineProps<{ state: State }>()
         </template>
       </Lang>
 
-      <div class="flex flex-col space-y-4">
-        <Lang class="text-2xl font-bold md:text-3xl">
-          <template #zh>开始</template>
-          <template #en>Start</template>
-        </Lang>
-
-        <div class="flex flex-col items-start space-y-2 text-xl md:text-2xl">
-          <button
-            class="flex space-x-2 whitespace-pre hover:underline disabled:text-stone-500 disabled:no-underline"
-            @click="stateDirectoryOpen(state)"
-          >
-            <FolderIcon class="h-7 w-7" />
-            <Lang>
-              <template #zh>打开文件夹</template>
-              <template #en>Open Directory</template>
-            </Lang>
-          </button>
-
-          <button
-            class="flex space-x-2 whitespace-pre hover:underline disabled:text-stone-500 disabled:no-underline"
-            @click="stateFileOpen(state)"
-          >
-            <DocumentTextIcon class="h-7 w-7" />
-            <Lang>
-              <template #zh>打开文件</template>
-              <template #en>Open File</template>
-            </Lang>
-          </button>
-
-          <button
-            class="flex space-x-2 whitespace-pre hover:underline disabled:text-stone-500 disabled:no-underline"
-            @click="stateFileCreate(state)"
-          >
-            <DocumentPlusIcon class="h-7 w-7" />
-            <Lang>
-              <template #zh>创建文件</template>
-              <template #en>Create File</template>
-            </Lang>
-          </button>
-        </div>
-      </div>
+      <EditorWelcomeStart :state="state" />
 
       <div class="flex flex-col space-y-4">
         <Lang class="text-2xl font-bold md:text-3xl">
