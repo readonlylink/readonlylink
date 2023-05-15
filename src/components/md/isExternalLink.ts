41e4c187646b1be4380aa509bef46ce70c4c6696
Xie Yuheng <xyheme@gmail.com>
2023-05-16 03:26:22 +0800

    up

--- src/components/md/isExternalLink.ts
+++ src/components/md/isExternalLink.ts
@@ -2,6 +2,7 @@ export function isExternalLink(link: string) {
   return (
     link.startsWith('http://') ||
     link.startsWith('https://') ||
+    link.startsWith('data:') ||
     link.startsWith('mailto:')
   )
 }
