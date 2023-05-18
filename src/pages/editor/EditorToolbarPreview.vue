df61b5644ae4b00fa93ca8813323f00384ab3046
Xie Yuheng <xyheme@gmail.com>
2023-05-18 19:30:29 +0800

    up

--- src/pages/editor/EditorToolbarPreview.vue
+++ src/pages/editor/EditorToolbarPreview.vue
@@ -11,35 +11,33 @@ const origin = useCurrentOrigin()
 </script>
 
 <template>
-  <div class="flex space-x-4">
-    <a
-      v-if="state.currentTab && state.currentTab.file.name.endsWith('.md')"
-      :href="`${origin}/articles/data:text/markdown;base64,${Base64.encode(
-        state.currentTab.text,
-      )}`"
-      class="flex items-center space-x-1 hover:underline"
-      target="_blank"
-    >
-      <PlayIcon class="h-5 w-5" />
-      <Lang>
-        <template #zh>预览 Markdown</template>
-        <template #en>Preview Markdown</template>
-      </Lang>
-    </a>
+  <a
+    v-if="state.currentTab && state.currentTab.file.name.endsWith('.md')"
+    :href="`${origin}/articles/data:text/markdown;base64,${Base64.encode(
+      state.currentTab.text,
+    )}`"
+    class="flex items-center space-x-1 hover:underline"
+    target="_blank"
+  >
+    <PlayIcon class="h-5 w-5" />
+    <Lang>
+      <template #zh>预览 Markdown</template>
+      <template #en>Preview Markdown</template>
+    </Lang>
+  </a>
 
-    <a
-      v-if="state.currentTab && state.currentTab.file.name.endsWith('.mimor')"
-      :href="`https://mimor.app/mimors/data:text/plain;base64,${Base64.encode(
-        state.currentTab.text,
-      )}`"
-      class="flex items-center space-x-1 hover:underline"
-      target="_blank"
-    >
-      <PlayIcon class="h-5 w-5" />
-      <Lang>
-        <template #zh>预览 Mimor</template>
-        <template #en>Preview Mimor</template>
-      </Lang>
-    </a>
-  </div>
+  <a
+    v-if="state.currentTab && state.currentTab.file.name.endsWith('.mimor')"
+    :href="`https://mimor.app/mimors/data:text/plain;base64,${Base64.encode(
+      state.currentTab.text,
+    )}`"
+    class="flex items-center space-x-1 hover:underline"
+    target="_blank"
+  >
+    <PlayIcon class="h-5 w-5" />
+    <Lang>
+      <template #zh>预览 Mimor</template>
+      <template #en>Preview Mimor</template>
+    </Lang>
+  </a>
 </template>
