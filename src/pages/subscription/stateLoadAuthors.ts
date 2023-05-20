477315e89add21f8900b9e3bf333971e8a0d4e43
Xie Yuheng <xyheme@gmail.com>
2023-05-21 00:35:20 +0800

    move `loadAuthors` to `models/`

--- src/pages/subscription/stateLoadAuthors.ts
+++ src/pages/subscription/stateLoadAuthors.ts
@@ -1,4 +1,4 @@
-import { loadAuthors } from './loadAuthors'
+import { loadAuthors } from '../../models/author/loadAuthors'
 import { State } from './State'
 
 export async function stateLoadAuthors(state: State): Promise<void> {
