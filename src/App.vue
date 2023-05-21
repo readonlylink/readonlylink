d07b2aa8c1aec89cc821d436277133a45008c171
Xie Yuheng <xyheme@gmail.com>
2023-05-21 22:50:44 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,3 @@
-<script setup lang="ts">
-import { Head } from '@vueuse/head'
-</script>
-
 <template>
-  <Head>
-    <meta name="theme-color" content="#fff" />
-    <meta name="msapplication-navbutton-color" content="#fff" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="#fff" />
-  </Head>
-
   <RouterView />
 </template>
