6b2a3eac6885d57637336eccfa8d44cba641b9cd
Xie Yuheng <xyheme@gmail.com>
2023-06-11 15:43:29 +0800

    up

--- src/reactives/useDefaultAuthorList.ts
+++ src/reactives/useDefaultAuthorList.ts
@@ -2,6 +2,7 @@ export function useDefaultAuthorList(): Array<string> {
   return [
     'https://readonly.link/contents/author.json',
     'https://inner.xieyuheng.com/author.json',
+    'https://shadowrz.github.io/author.json',
     'https://cdn.cic.run/docs/author.json',
     'https://cdn.fidb.dev/docs/author.json',
     'https://mimor.app/contents/author.json',
