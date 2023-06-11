4d9df383fd8b98a1e35cbfdebca58ee29339db9a
Xie Yuheng <xyheme@gmail.com>
2023-06-11 15:42:26 +0800

    remove unmaintained author link
    - 'https://shadowrz.github.io/author.json'

--- src/reactives/useDefaultAuthorList.ts
+++ src/reactives/useDefaultAuthorList.ts
@@ -2,7 +2,6 @@ export function useDefaultAuthorList(): Array<string> {
   return [
     'https://readonly.link/contents/author.json',
     'https://inner.xieyuheng.com/author.json',
-    'https://shadowrz.github.io/author.json',
     'https://cdn.cic.run/docs/author.json',
     'https://cdn.fidb.dev/docs/author.json',
     'https://mimor.app/contents/author.json',
