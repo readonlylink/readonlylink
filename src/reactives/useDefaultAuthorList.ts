f3042b9d9c97fed3b52fc835a11e0b2a30ab85e5
Xie Yuheng <xyheme@gmail.com>
2023-09-15 09:43:38 +0800

    up

--- src/reactives/useDefaultAuthorList.ts
+++ src/reactives/useDefaultAuthorList.ts
@@ -4,7 +4,7 @@ export function useDefaultAuthorList(): Array<string> {
     'https://inner.xieyuheng.com/author.json',
     'https://shadowrz.github.io/author.json',
     'https://code-of-cicada.fidb.app/docs/author.json',
-    'https://cdn.fidb.dev/docs/author.json',
+    'https://code-of-fidb.fidb.app/docs/author.json',
     'https://mimor.app/contents/author.json',
   ]
 }
