4f1d4bb08dc1f6da6c7a9da78c44f1072fd1a89d
Xie Yuheng <xyheme@gmail.com>
2023-10-05 23:27:49 +0800

    [page-layout] PageFoot -- link to Xie Yuheng Software Studio

--- TODO.md
+++ TODO.md
@@ -1,7 +1,5 @@
 [author] author can provide different logos for light vs dark
 [author] author can provide different contents for different languages
-
-[page-layout] PageFoot -- link to Xie Yuheng Software Studio
 [manual] link to readonly.link
 [book] link to readonly.link -- from title page only
 [article] link to readonly.link -- from foot
--- src/layouts/page-layout/PageLayout.vue
+++ src/layouts/page-layout/PageLayout.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Head } from '@vueuse/head'
 import { useGlobalLang } from '../../components/lang/useGlobalLang'
+import PageFoot from './PageFoot.vue'
 import PageHead from './PageHead.vue'
 
 const lang = useGlobalLang()
@@ -10,17 +11,27 @@ const lang = useGlobalLang()
   <div
     class="h-screen-dynamic overflow-auto bg-white dark:bg-black text-black dark:text-white"
   >
+    <Head>
+      <title v-if="lang.isZh()">只读</title>
+      <title v-else>Readonly</title>
+    </Head>
+
     <div
-      class="mx-auto flex max-w-3xl flex-col px-3 py-3 text-xl sm:px-6 sm:py-4"
+      class="mx-auto min-h-full h-full justify-between flex max-w-3xl flex-col px-3 py-3 text-xl sm:px-6 sm:py-4"
     >
-      <Head>
-        <title v-if="lang.isZh()">只读</title>
-        <title v-else>Readonly</title>
-      </Head>
+      <div>
+        <PageHead />
 
-      <PageHead />
+        <div class="flex flex-col">
+          <slot />
+        </div>
+      </div>
 
-      <slot />
+      <div class="pt-20">
+        <PageFoot
+          class="border-t dark:border-white border-black py-2"
+        />
+      </div>
     </div>
   </div>
 </template>
