f2f0db0496bab853a2b36b49d8d36070af389308
Xie Yuheng <xyheme@gmail.com>
2023-10-06 04:49:47 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { Head } from '@vueuse/head'
+import { useGlobalLang } from './components/lang/useGlobalLang'
+import { useGlobalTheme } from './models/theme'
+
+const lang = useGlobalLang()
+const theme = useGlobalTheme()
+</script>
+
 <template>
+  <Head>
+    <title v-if="lang.isZh()">Readonly</title>
+    <title v-else>Readonly</title>
+
+    <meta v-if="theme.name === 'light'" name="theme-color" content="white" />
+    <meta v-if="theme.name === 'dark'" name="theme-color" content="black" />
+  </Head>
+
   <RouterView />
 </template>
