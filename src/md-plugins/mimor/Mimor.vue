36155e397375d4de2bfc148eb5945b5a64cd4f8c
Xie Yuheng <xyheme@gmail.com>
2024-10-29 19:11:14 +0800

    x-mimor -- use lang and theme

--- src/md-plugins/mimor/Mimor.vue
+++ src/md-plugins/mimor/Mimor.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import 'mimor'
+import { useGlobalLang } from '../../components/lang/useGlobalLang'
+import { useGlobalTheme } from '../../models/theme'
 
-defineProps<{
-  src: string
-}>()
+defineProps<{ src: string }>()
+
+const lang = useGlobalLang()
+const theme = useGlobalTheme()
 </script>
 
 <template>
   <div class="h-[36rem] w-full sm:w-[36rem]" title="An embedded mimor.">
-    <x-mimor class="h-full" :src="src" />
+    <x-mimor
+      class="h-full"
+      :src="src"
+      :lang-tag="lang.tag"
+      :theme-name="theme.name"
+    />
   </div>
 </template>
